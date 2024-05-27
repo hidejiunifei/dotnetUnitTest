9d930974090238ace860020476e7c63d81fbe9a1
hidejiunifei <hidejiunifei@gmail.com>
2024-05-27 03:39:53 -0300

    Add files via upload

---
+++ GenerateUnitTest/Extension.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenerateUnitTest
+{
+    public static class Extension
+    {
+        public static ClassDeclarationSyntax AddConstructorDeclaration(this ClassDeclarationSyntax syntax, string className, IEnumerable<ParameterSyntax> param)
+        {
+            return syntax.AddMembers(SyntaxFactory.ConstructorDeclaration($"{className}Tests")
+                                .AddModifiers(SyntaxFactory.Token(SyntaxKind.PublicKeyword))
+                                .WithBody(SyntaxFactory.Block(
+            new SyntaxList<StatementSyntax>(
+                                    param.Select(x => SyntaxFactory.ExpressionStatement(
+                                        SyntaxFactory.AssignmentExpression(SyntaxKind.SimpleAssignmentExpression,
+                                        SyntaxFactory.IdentifierName($"_mock{x.Identifier.Text.Substring(0, 1).ToUpper()}{x.Identifier.Text.Substring(1)}"),
+                                        SyntaxFactory.ObjectCreationExpression(SyntaxFactory.GenericName(SyntaxFactory.ParseToken("Mock"),
+                                            SyntaxFactory.TypeArgumentList(
+                                                SyntaxFactory.SingletonSeparatedList<TypeSyntax>(
+                                                SyntaxFactory.IdentifierName(x.Type.ToString())))), SyntaxFactory.ArgumentList(), null)
+                                        ))).ToArray()).
+                                        Add(SyntaxFactory.ExpressionStatement(SyntaxFactory.AssignmentExpression(SyntaxKind.SimpleAssignmentExpression,
+                                        SyntaxFactory.IdentifierName($"_{className}"), SyntaxFactory.ObjectCreationExpression(SyntaxFactory.ParseTypeName(className),
+                                        SyntaxFactory.ArgumentList(
+            SyntaxFactory.SeparatedList<ArgumentSyntax>(
+                                                param.Select(x => SyntaxFactory.Argument(
+                                                    SyntaxFactory.IdentifierName($"_mock{x.Identifier.Text.Substring(0, 1).ToUpper()}{x.Identifier.Text.Substring(1)}.Object")
+                                                )).ToArray())
+                                            )
+                                        , null))))
+                                        )
+                                    ));
+        }
+
+        public static NamespaceDeclarationSyntax AddClassDeclaration(this NamespaceDeclarationSyntax syntax, string className, IEnumerable<ParameterSyntax> param)
+        {
+            return syntax.AddMembers(
+                                SyntaxFactory.ClassDeclaration($"{className}Tests")
+                                .AddModifiers(SyntaxFactory.Token(SyntaxKind.PublicKeyword))
+                                .AddMembers(SyntaxFactory.FieldDeclaration(SyntaxFactory.VariableDeclaration(SyntaxFactory.ParseTypeName(className)).
+                                        AddVariables(SyntaxFactory.VariableDeclarator($"_{className}"))
+                                    ).AddModifiers(SyntaxFactory.Token(SyntaxKind.PrivateKeyword), SyntaxFactory.Token(SyntaxKind.ReadOnlyKeyword)))
+                                .AddMembers(
+                                    param.Select(x => SyntaxFactory.FieldDeclaration(
+                                            SyntaxFactory.VariableDeclaration(SyntaxFactory.GenericName(SyntaxFactory.ParseToken("Mock"),
+                                            SyntaxFactory.TypeArgumentList(
+                                                SyntaxFactory.SingletonSeparatedList<TypeSyntax>(
+                                                SyntaxFactory.IdentifierName(x.Type.ToString())))))
+                                            .AddVariables(
+                                            SyntaxFactory.VariableDeclarator($"_mock{x.Identifier.Text.Substring(0, 1).ToUpper()}{x.Identifier.Text.Substring(1)}"))
+                                        ).AddModifiers(SyntaxFactory.Token(SyntaxKind.PrivateKeyword), SyntaxFactory.Token(SyntaxKind.ReadOnlyKeyword))).ToArray()
+                                  ).AddConstructorDeclaration(className, param));
+        }
+    }
+}
---
+++ GenerateUnitTest/GenerateUnitTests.cs
@@ -0,0 +1,130 @@
+﻿using EnvDTE;
+using Microsoft.VisualStudio.Shell;
+using System;
+using System.ComponentModel.Design;
+using Task = System.Threading.Tasks.Task;
+using EnvDTE80;
+using Microsoft.CodeAnalysis.CSharp;
+using System.IO;
+using System.Linq;
+using static System.Net.Mime.MediaTypeNames;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System.Windows.Input;
+using Newtonsoft.Json.Linq;
+
+namespace GenerateUnitTest
+{
+    /// <summary>
+    /// Command handler
+    /// </summary>
+    internal sealed class GenerateUnitTests
+    {
+        /// <summary>
+        /// Command ID.
+        /// </summary>
+        public const int CommandId = 0x0100;
+
+        /// <summary>
+        /// Command menu group (command set GUID).
+        /// </summary>
+        public static readonly Guid CommandSet = new Guid("7d642241-da32-480e-9fbd-a9bfb78aca52");
+
+        /// <summary>
+        /// VS Package that provides this command, not null.
+        /// </summary>
+        private readonly AsyncPackage package;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GenerateUnitTests"/> class.
+        /// Adds our command handlers for menu (commands must exist in the command table file)
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        /// <param name="commandService">Command service to add command to, not null.</param>
+        private GenerateUnitTests(AsyncPackage package, OleMenuCommandService commandService)
+        {
+            this.package = package ?? throw new ArgumentNullException(nameof(package));
+            commandService = commandService ?? throw new ArgumentNullException(nameof(commandService));
+
+            var menuCommandID = new CommandID(CommandSet, CommandId);
+            var menuItem = new MenuCommand(this.Execute, menuCommandID);
+            commandService.AddCommand(menuItem);
+        }
+
+        /// <summary>
+        /// Gets the instance of the command.
+        /// </summary>
+        public static GenerateUnitTests Instance
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Gets the service provider from the owner package.
+        /// </summary>
+        private Microsoft.VisualStudio.Shell.IAsyncServiceProvider ServiceProvider
+        {
+            get
+            {
+                return this.package;
+            }
+        }
+
+        /// <summary>
+        /// Initializes the singleton instance of the command.
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        public static async Task InitializeAsync(AsyncPackage package)
+        {
+            // Switch to the main thread - the call to AddCommand in GenerateHandlerUnitTests's constructor requires
+            // the UI thread.
+            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync(package.DisposalToken);
+
+            OleMenuCommandService commandService = await package.GetServiceAsync(typeof(IMenuCommandService)) as OleMenuCommandService;
+            Instance = new GenerateUnitTests(package, commandService);
+        }
+
+        /// <summary>
+        /// This function is the callback used to execute the command when the menu item is clicked.
+        /// See the constructor to see how the menu item is associated with this function using
+        /// OleMenuCommandService service and MenuCommand class.
+        /// </summary>
+        /// <param name="sender">Event sender.</param>
+        /// <param name="e">Event args.</param>
+        private async void Execute(object sender, EventArgs e)
+        {
+            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
+            // Get DTE Object
+            var dte = Package.GetGlobalService(typeof(DTE)) as DTE2;
+            UIHierarchy uih = dte.ToolWindows.SolutionExplorer;
+
+            Array selectedItems = (Array)uih.SelectedItems;
+            if (null != selectedItems)
+            {
+                foreach (UIHierarchyItem selItem in selectedItems)
+                {
+                    ProjectItem prjItem = selItem.Object as ProjectItem;
+                    string filePath = prjItem.Properties.Item("FullPath").Value.ToString();
+                    var tree = CSharpSyntaxTree.ParseText(File.ReadAllText(filePath));
+                    var nameSpace = tree.GetRoot().ChildNodes().Single(x => x.Kind() == SyntaxKind.NamespaceDeclaration);
+                    var classDeclaration = ((ClassDeclarationSyntax)nameSpace.ChildNodes().Single(x => x.Kind() == SyntaxKind.ClassDeclaration)).WithModifiers(SyntaxTokenList.Create(SyntaxFactory.Token(SyntaxKind.PublicKeyword)));
+
+                    if (classDeclaration.Modifiers.Any(x => x.Kind() != SyntaxKind.PublicKeyword))
+                        continue;
+                    var constructor = ((ConstructorDeclarationSyntax)classDeclaration.ChildNodes().Single(z => z.Kind() == SyntaxKind.ConstructorDeclaration)).WithModifiers(SyntaxTokenList.Create(SyntaxFactory.Token(SyntaxKind.PublicKeyword)));
+
+                    if (constructor.Modifiers.Any(x => x.Kind() != SyntaxKind.PublicKeyword))
+                        continue;
+
+                    var comp = SyntaxFactory.CompilationUnit()
+                        .AddMembers(
+                        SyntaxFactory.NamespaceDeclaration(SyntaxFactory.IdentifierName($"{((NamespaceDeclarationSyntax)nameSpace).Name}"))
+                            .AddClassDeclaration(classDeclaration.Identifier.Text, constructor.ParameterList.ChildNodes().Cast<ParameterSyntax>())
+                        ).NormalizeWhitespace().ToFullString();
+                    File.WriteAllText(filePath.Replace(".cs", "Tests.cs"), comp);
+                }
+            }
+        }
+    }
+}
